f33388d76d4d4f838bc440daaa0e4e4886c3352e
Jochyd <mail>
2022-10-13 21:41:46 -0300

    agregue el listado de noticias

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
 import { Container, Form, Button } from "react-bootstrap";
 import ListaNoticias from "./ListaNoticias";
 
 const Formulario = () => {
+  const [noticia, setNoticia] = useState([]);
+  const [categorias, setCategorias] = useState ([]);
+    
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
+  let consultarAPI = async () => {
+      try{
+        let respuesta = await fetch('https://newsdata.io/api/1/news?country=ar&apikey=pub_122296e4966d2de81e7b449960696484df6ad&country=ar&category=sports,business,food,politics');
+        let dato = await respuesta.json();
+        let listaNoticias = dato.results;
+        let listaCategoria = [...new Set(listaNoticias.map((noticia)=>noticia.category))];
+        console.log(listaCategoria);
+        let categoria = [...listaCategoria];
+        console.log(categoria);
+        setNoticia(listaNoticias);
+
+    }catch(error){
+        console.log(error);
+        //agregar cartelito  error
+    }
+  };
 
   return (
     <>
